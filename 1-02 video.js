61929d6619d8c52cdca0a044d1a7235055d9b5ea
04enrique <129766353+04enrique@users.noreply.github.com>
2023-04-05 22:59:11 -0400

    1-02 video
    1-02 video

---
+++ 1-02 video.js
@@ -0,0 +1,35 @@
+var edad = 23;
+
+var punto_flotantes = 12.5;
+
+
+/*
+Operadores aritmeticos
+Division = /
+Multiplicacion = *
+Suma = +
+Resta = -
+Modulo = %
+*/
+
+var resultado_suma = edad % 2;
+
+console.log(resultado_suma);
+
+
+
+
+
+console.log(resultado_suma);
+
+Function es_par(numero){
+  if(numero % 2 == 0){
+    return true;
+  }
+  return false;
+}
+
+
+
+
+console.log(Math.sqrt(4));
